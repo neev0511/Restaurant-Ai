eecb792be1753daebe57bcff0fcbe54018d66153
neev0511 <67385225+neev0511@users.noreply.github.com>
2021-07-24 13:17:36 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,142 @@
+from tkinter import *
+import time
+import tkinter.messagebox
+from bot import chat
+import pyttsx3
+import threading
+
+saved_username = ["You"]
+#ans=["PyBot"]
+window_size="400x400"
+
+class ChatInterface(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        # sets default bg for top level windows
+        self.tl_bg = "#EEEEEE"
+        self.tl_bg2 = "#EEEEEE"
+        self.tl_fg = "#000000"
+        self.font = "Verdana 10"
+
+
+        self.text_frame = Frame(self.master, bd=6)
+        self.text_frame.pack(expand=True, fill=BOTH)
+
+        # scrollbar for text box
+        self.text_box_scrollbar = Scrollbar(self.text_frame, bd=0)
+        self.text_box_scrollbar.pack(fill=Y, side=RIGHT)
+
+        # contains messages
+        self.text_box = Text(self.text_frame, yscrollcommand=self.text_box_scrollbar.set, state=DISABLED,
+                             bd=1, padx=6, pady=6, spacing3=8, wrap=WORD, bg=None, font="Verdana 10", relief=GROOVE,
+                             width=10, height=1)
+        self.text_box.pack(expand=True, fill=BOTH)
+        self.text_box_scrollbar.config(command=self.text_box.yview)
+
+        # frame containing user entry field
+        self.entry_frame = Frame(self.master, bd=1)
+        self.entry_frame.pack(side=LEFT, fill=BOTH, expand=True)
+
+        # entry field
+        self.entry_field = Entry(self.entry_frame, bd=1, justify=LEFT)
+        self.entry_field.pack(fill=X, padx=6, pady=6, ipady=3)
+        # self.users_message = self.entry_field.get()
+
+        # frame containing send button and emoji button
+        self.send_button_frame = Frame(self.master, bd=0)
+        self.send_button_frame.pack(fill=BOTH)
+
+        # send button
+        self.send_button = Button(self.send_button_frame, text="Send", width=5, relief=GROOVE, bg='white',
+                                  bd=1, command=lambda: self.send_message_insert(None), activebackground="#FFFFFF",
+                                  activeforeground="#000000")
+        self.send_button.pack(side=LEFT, ipady=8)
+        self.master.bind("<Return>", self.send_message_insert)
+        #t2 = threading.Thread(target=self.send_message_insert(, name='t1')
+        #t2.start()
+
+        #print("Played Successfully......")
+
+    def clear_chat(self):
+        self.text_box.config(state=NORMAL)
+        self.text_box.delete(1.0, END)
+        self.text_box.delete(1.0, END)
+        self.text_box.config(state=DISABLED)
+
+    def chatexit(self):
+        exit()
+
+    def send_message_insert(self, message):
+        user_input = self.entry_field.get()
+        pr1 = "You : " + user_input + "\n"
+        self.text_box.configure(state=NORMAL)
+        self.text_box.insert(END, pr1)
+        self.text_box.configure(state=DISABLED)
+        self.text_box.see(END)
+        #t1 = threading.Thread(target=self.playResponce, args=(user_input,))
+        #t1.start()
+        #time.sleep(1)
+        ob=chat(user_input)
+
+        pr="Bot : " + ob + "\n"
+        self.text_box.configure(state=NORMAL)
+        self.text_box.insert(END, pr)
+        self.text_box.configure(state=DISABLED)
+        self.text_box.see(END)
+        self.entry_field.delete(0,END)
+        time.sleep(0)
+
+    def font(self):
+        self.text_box.config(font="Verdana 10")
+        self.entry_field.config(font="Verdana 10")
+        self.font = "Verdana 10"
+
+    # Blue
+    def color_theme(self):
+        self.master.config(bg="#263b54")
+        self.text_frame.config(bg="#263b54")
+        self.text_box.config(bg="#1c2e44", fg="#FFFFFF")
+        self.entry_frame.config(bg="#263b54")
+        self.entry_field.config(bg="#1c2e44", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#263b54")
+        self.send_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+
+        self.tl_bg = "#1c2e44"
+        self.tl_bg2 = "#263b54"
+        self.tl_fg = "#FFFFFF"
+
+
+
+
+    # Default font and color theme
+    def default_format(self):
+        self.master.config(bg="#263b54")
+        self.text_frame.config(bg="#263b54")
+        self.text_box.config(bg="#1c2e44", fg="#FFFFFF")
+        self.entry_frame.config(bg="#263b54")
+        self.entry_field.config(bg="#1c2e44", fg="#FFFFFF", insertbackground="#FFFFFF")
+        self.send_button_frame.config(bg="#263b54")
+        self.send_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+        #self.emoji_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
+
+        self.tl_bg = "#1c2e44"
+        self.tl_bg2 = "#263b54"
+        self.tl_fg = "#FFFFFF"
+        self.text_box.config(font="Verdana 10")
+        self.entry_field.config(font="Verdana 10")
+        self.font = "Verdana 10"
+
+
+root=Tk()
+
+
+a = ChatInterface(root)
+root.geometry(window_size)
+root.title("BudBot")
+root.iconbitmap('i.ico')
+a.default_format()
+root.mainloop()
